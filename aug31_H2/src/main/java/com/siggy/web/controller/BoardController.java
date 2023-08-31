41afb78061363e4cd2e22cba72c05e6544cf964b
siggy <siggy@BOOK-8V7IG0LIJL>
2023-08-31 19:56:12 +0900

    ThymeLeaf

---
+++ aug31_H2/src/main/java/com/siggy/web/controller/BoardController.java
@@ -0,0 +1,26 @@
+package com.siggy.web.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.siggy.web.service.BoardService;
+
+@Controller
+public class BoardController {
+	@Autowired
+	private BoardService boardService;
+	
+	@GetMapping("/board")
+	public String board(Model model) {
+		List<Map<String, Object>> list = boardService.list();
+		System.out.println(list);
+		
+		model.addAttribute("list", list);
+		return "/board";
+	}
+}
---
+++ aug31_H2/src/main/java/com/siggy/web/service/BoardService.java
@@ -0,0 +1,20 @@
+package com.siggy.web.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.siggy.web.dao.BoardDAO;
+
+@Service
+public class BoardService {
+	@Autowired
+	private BoardDAO boardDAO;
+
+	public List<Map<String, Object>> list() {
+		return boardDAO.list();
+	}
+
+}
